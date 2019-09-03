6a80ac7f7f8cddd4da7b8de8ce8574752fa06682
cudjoeab <cudjoeab@gmail.com>
2019-09-03 19:32:36 -0400

    created forEach loop with a callback to convert the nested array to an object

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function arrayToObj() {
-   // your code goes here  
+function arrayToObj(array) {
+   console.log(array)
+   let arrayToObj = {};
+   
+   
+   array.forEach(e=>
+
+      arrayToObj[e[0]] = e[1]
+      
+   );
+   
+
+   console.log(arrayToObj)
+   return arrayToObj; 
+   
 }
+ 
+
 
 module.exports = arrayToObj;
